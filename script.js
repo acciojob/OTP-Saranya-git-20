4cc21afc7228be21a624310e25b7df739c20487d
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-07-27 22:28:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+    inputs[0].focus();
+
+    inputs.forEach((input, index) => {
+      input.addEventListener('input', (e) => {
+        const value = e.target.value;
+        if (value && index < inputs.length - 1) {
+          inputs[index + 1].focus();
+        }
+      });
+
+      input.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace') {
+          if (e.target.value === '' && index > 0) {
+            inputs[index - 1].focus();
+            inputs[index - 1].value = '';
+          }
+        } else if (e.key >= '0' && e.key <= '9') {
+          // allow number
+        } else if (e.key !== 'Tab') {
+          e.preventDefault();
+        }
+      });
+    });
+  
